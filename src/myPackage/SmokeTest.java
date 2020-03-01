04d28d026ea8748eb46a356cd79dfaf402f74316
NikhilDev203 <31949986+NikhilDev203@users.noreply.github.com>
2020-03-01 21:41:18 +0530

    Update SmokeTest.java

--- src/myPackage/SmokeTest.java
+++ src/myPackage/SmokeTest.java
@@ -15,7 +15,6 @@ public class SmokeTest {
 		WebDriver driver = new FirefoxDriver();
 		driver.get("http:\\rediff.com");
 		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		
 		driver.findElement(By.xpath("//*[@id='signin_info']/a[1]")).click();
 		
